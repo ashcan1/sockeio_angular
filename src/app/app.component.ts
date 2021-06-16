60fbedbbf8299ccedbb7b9751412a12817a3b8fb
Angelo <angelo@osradar.com>
2021-06-16 08:56:33 +0100

    as

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { MatTableDataSource } from '@angular/material/table';
 import { SocketService } from './socket.service';
 
 
-
 export interface PeriodicElement {
   MSTID: number;
   Match: number;
@@ -11,6 +10,8 @@ export interface PeriodicElement {
   Last: string;
   Nationality: string;
 }
+
+// test delete
 const ELEMENT_DATA: PeriodicElement[] = [
   {MSTID: 36867, Match: 16, First: "Jorge", Last: "Campillo", Nationality: "ESP",},
   {MSTID: 36867, Match: 16, First: "Jorge", Last: "Campillo", Nationality: "ESP",},
@@ -29,13 +30,13 @@ export class AppComponent implements OnInit {
   title = 'GolfTournament';
   displayedColumns: string[] = ['MSTID', 'Match', 'First', 'Last', 'Nationality'];
   dataSource = new MatTableDataSource<PeriodicElement>([]);
-
+ //inject the service
   constructor(private socketService: SocketService) {}
-
+// connect to the socket
   ngOnInit() {
     this.getPlayers();
   }
-
+// listen to the event from service
   getPlayers() {
     this.socketService.listenToPlayerEvent('data-update');
     this.socketService.getPlayers()
--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -1,22 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Observable, observable, Subscriber, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import {io} from 'socket.io-client';
 const socketUrl = 'https://mst-full-stack-dev-test.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
-export class SocketService {
+  export class SocketService {
   socket: any;
-  players$ = new BehaviorSubject<any[]>([])
+  players$ = new BehaviorSubject<any>([])
 
   constructor() {
     this.socket = io(socketUrl);
    }
-    
+
    listenToPlayerEvent(eventName: string) {
     this.socket.on(eventName, (data: any) => {
-      console.log('DATA ==', data);
-      this.setPlayer(data);
+            this.setPlayer(data);
     });
    }
 
@@ -27,7 +26,6 @@ export class SocketService {
    setPlayer(player: any) {
      const players = this.players$.value;
      players.push(player);
-     console.log('set Players ==>', players);
      this.players$.next(players);
    }
 
